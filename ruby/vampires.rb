35ef208429899233a9f61f6aab7797f8656a305b
Julia Greenwald <julia.greenwald@gmail.com>
2017-01-26 15:21:36 -0800

    add question data to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "what's your name?"
+your_name = gets.chomp
+
+puts "how old are you? what year were you born?"
+age_year = gets.chomp
+
+puts "can i get you some garlic bread?"
+garlic_bread = gets.chomp
+
+puts "would you like health insurance?"
+health_ins = gets.chomp 

ee624e0d1c81e3a05dd3c447d2ed363b71742d5e
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-05 18:34:55 -0800

    add code to change convert name into a coded name and notes on vowel/consonant issues

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,50 @@
+# a few notes: 
+#i ran out of time and didn't have a chance to figure out how to identify and change the vowels and consonants
+#i tried to put in some code that would display the overall vowel and consonant counts but it kept crashing the program so i had to comment it out
+
 #set up method for reordering the name - clunky but it works
 
-#set up method for changing vowels
+#set up method for changing vowels & consonants
+	#set up my vowels in a string or array that i can look through? 
+	#map and .next 
+	#could i split on vowels? consonants? 
+	# how do i remove the ! from the current output
 
-#set up method for changing consonants
-
-#def fake_name (first_name, last_name)
-	puts "enter first name & hit enter. then enter last name & hit enter"
+loop do
+	puts "enter first name & hit enter. then enter last name & hit enter. to exit, type 'quit'"
 	first_name = gets.chomp
 	last_name = gets.chomp
 	
+	break if first_name == "quit" || last_name == "quit"
 
-	p first_name.prepend ("#{last_name} ")
+	full_name = first_name.prepend ("#{last_name} ")
+
+	p full_name
+	
+	
+	#puts "the number of vowels in your name is: " full_name.count ("aeiou")
+	
+
+	
+	#puts "the number of consonants in your name is: " full_name.count ("bcdfghjklmqprstvwxyz")
+		
+	
+
+	vowel = ["a", "e", "i", "o", "u"]
+	new_vowels = "#{full_name}".split ("")
+	p new_vowels
+
+	p new_vowels.map! { |vowel| vowel.next }
+
+	name2 = new_vowels.join ("")
+
+	p "your coded name is: #{name2}" 
+	
+end 
+
+	
+
+
+
+
 	

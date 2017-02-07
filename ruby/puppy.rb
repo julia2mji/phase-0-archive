1ac0eb0f0ae939d8b18e8771c6af354e01933c57
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-07 14:01:08 -0800

    add puppy type characteristics, personality, etc.

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Puppy
+
+#Type 
+#Corgi
+
+#Characteristics
+#Height: 10 to 12 in
+#Max Weight: 30 lbs 
+#Colors: Red, Sable, Black, tri-color, fawn, with white markings. 
+
+#Personality/Behavior 
+#Happy
+#Loving
+#Intelligent
+#Independent mind/spirit 
+#Love to eat 

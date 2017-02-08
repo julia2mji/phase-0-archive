3b59a70c3f778bbeac8f38ecf3540aea5771d678
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-07 19:00:19 -0800

    add attributes and driver code to santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,17 @@
-class Santa
-	def initialize
-		puts "Initializing Santa instance ..."
+class Santa 
+	def initialize (gender, ethnicity)
+		#puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
 	end 
+
+	def reindeer_ranking
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end 
+
+	def age
+		age = 0
+	end
 	
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -10,9 +20,11 @@ class Santa
 	def eat_milk_and_cookies (cookie_type)
 		puts "That was a good #{cookie_type} cookie"
 	end 
+
+
 	
 end 
 
-santaing = Santa.new
+santaing = Santa.new ("agender", "Japanese")
 santaing.speak
 santaing.eat_milk_and_cookies ("shortbread")	

55e53f3b031dc825abb1212b7851a3f8c3200a45
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-11 15:35:06 -0800

    add modules, methods, and initial driver code to shout.rb

---
+++ ruby/shout.rb
@@ -0,0 +1,15 @@
+module Shout
+  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+  	words + "!!!" + " :("
+  end
+  
+  def self.yell_happily(words)
+  	words + "~~" + " :)"
+  end		
+end
+
+# :yell_angrily("you were late") 
+# :yell_happily("you were late
+#having trouble with my driver code
+

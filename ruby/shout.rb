79d624cea0c362116055e94d086247e2c9a9076d
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-11 16:03:51 -0800

    add shout mixin module and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,38 @@
-module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
-  	words + "!!!" + " :("
-  end
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+#   def self.yell_angrily(words)
+#   	words + "!!!" + " :("
+#   end
   
-  def self.yell_happily(words)
-  	words + "~~" + " :)"
-  end		
-end
+#   def self.yell_happily(words)
+#   	words + "~~" + " :)"
+#   end		
+# end
 
 # :yell_angrily("you were late") 
 # :yell_happily("you were late
 #having trouble with my driver code
 
+module Shout
+	def yell_angrily(words)
+  	puts "#{words}" + "!!!" + " :("
+  	end
+
+  	def yell_happily(words)
+  	puts "#{words}" + "~~" + " :)"
+  	end
+end
+
+  class Man
+  	include Shout
+  end 
+  	
+  class Woman
+  	include Shout 
+  end
+
+  man = Man.new
+  man.yell_happily("you were late")
+
+  woman = Woman.new
+  woman.yell_angrily("you were late")		 	
